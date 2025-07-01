f757a7351e0d256eda3c7fc4f0ec19bca6eab230
leena ghatiya <leena@valuebound.com>
2025-07-01 11:44:29 +0530

    email, and phone signup signin

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyBoCauxrpkHo3kzCHq1A_Zi9YF63OerM18",
+      authDomain: "nxthyre.firebaseapp.com",
+      projectId: "nxthyre",
+      storageBucket: "nxthyre.firebasestorage.app",
+      messagingSenderId: "863630644667",
+      appId: "1:863630644667:web:137f60cd0f723ee301e09e",
+      measurementId: "G-WNJZH2TFC5"
+    };
