c166a73464c610d7ec96c5ba512aa595c3d65ba1
leena ghatiya <leena@valuebound.com>
2025-07-04 10:49:44 +0530

    changed the api endpoint to the hosted one

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,3 +7,7 @@ const firebaseConfig = {
       appId: "1:863630644667:web:137f60cd0f723ee301e09e",
       measurementId: "G-WNJZH2TFC5"
     };
+  
+// config.js
+const API_BASE_URL = "https://nxthyre-server-staging-863630644667.asia-south1.run.app/"; // Replace with your endpoint
+export { API_BASE_URL };
