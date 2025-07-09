45e25f7f958d89f9da74f7f3676be87e452b3300
leena ghatiya <leena@valuebound.com>
2025-07-09 12:19:00 +0530

    changed the backend url

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,7 +9,7 @@ export const firebaseConfig = {
     };
 
 // config.js
-// export const API_BASE_URL = "https://nxthyre-server-staging-863630644667.asia-south1.run.app"; // Replace with your endpoint
+export const API_BASE_URL = "https://nxthyre-server-staging-863630644667.asia-south1.run.app"; // Replace with your endpoint
 // export const API_BASE_URL = "https://41f3-2401-4900-1ca2-c03f-2414-a794-4b62-9ab2.ngrok-free.app";
 // export const API_BASE_URL = "http://127.0.0.1:8000";
-export const API_BASE_URL = "https://wnmm1w18-8000.inc1.devtunnels.ms"
+// export const API_BASE_URL = "https://wnmm1w18-8000.inc1.devtunnels.ms"
